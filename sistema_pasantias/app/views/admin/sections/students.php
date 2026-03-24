e72d533ad3d76d1b7a2a6b2d01d5ca1d0dad74c0
Misael Santana <franklinmisaelsantanamora045@gmail.com>
2026-03-24 10:51:04 -0400

    Ultimo commit, Gracias a dios

--- sistema_pasantias/app/views/admin/sections/students.php
+++ sistema_pasantias/app/views/admin/sections/students.php
@@ -79,7 +79,7 @@ $followUpMeta = [
             </select>
         </div>
         <button type="submit" class="card-btn" style="width:auto;padding:14px 20px;"><i class="fas fa-filter"></i> Filtrar</button>
-        <a href="index.php?page=admin-students" class="secondary-inline-btn" style="width:auto;padding:14px 20px;"><i class="fas fa-rotate-left"></i> Limpiar</a>
+        <a href="index.php?page=admin-students" class="company-secondary-btn" style="width:auto;padding:14px 20px;"><i class="fas fa-rotate-left"></i> Limpiar</a>
         <button
             type="submit"
             form="delete-all-students-form"
